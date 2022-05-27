09e8f8d90c3d870fd689d5267cd71b9523c76f75
Lutando Nyembezi <18001327@vcconnect.co.za>
2022-05-27 09:29:00 +0200

    Add project files.

---
+++ app_6/ContextInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_6_1
+{
+    class ContextInitializer : CreateDatabaseIfNotExists<SocerContext>                               
+    {
+        protected override void Seed(SocerContext sc)
+        {
+            Team t = new Team { TeamName = "Not defined", Coach = "Not defined" };
+            sc.Teams.Add(t);
+            sc.SaveChanges();
+        }
+
+    }
+
+}
---
+++ app_6/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_6_1
+{
+    public class Player
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public Posicion pos{ set; get; }
+
+        public virtual Team Team { set; get; }    
+        public int? TeamId { set; get; }        
+
+        public override string ToString()      
+        {
+            return Name;
+        }
+
+    }
+}
